71a2b9b2b4c34347307718b72b776c4a21fe49c0
bchollid <brandonholliday1@gmail.com>
2024-01-15 12:49:28 -0700

    Updated With Week 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,122 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int total = 0;
+            for(var i = 0; i<numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    total += numbers[i];
+                }
+                else if (numbers[i] % 2 != 0)
+                {
+                    total -= numbers[i];
+                }
+            }
+            return total;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            string[] stringArray = new string[] { str1, str2, str3, str4 };
+            int smallestLength = stringArray.Min(str => str.Length);
+            return smallestLength;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] intArray = new int[] { number1, number2, number3, number4 };
+            int smallestNumber = intArray.Min(num => num);
+            return smallestNumber;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if(sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            bool isItANumber = double.TryParse(input, out double number);
+            return isItANumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var tally = 0;
+            var majory = (objs.Length / 2) + 1;
+            for(int i = 0; i<objs.Length; i++)
+            {
+                if (objs[i] == null)
+                {
+                    tally++;
+                }
+            }
+
+            if(tally >= majory)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+            double count = 0;
+
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
+                return sum / count;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            int result = 1;
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for (int i = number; i>0; i--)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
